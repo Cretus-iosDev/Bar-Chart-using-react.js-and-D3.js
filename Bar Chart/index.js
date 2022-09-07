11418a2690aafaed6fa23fbb90ae7ce62703536c
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2022-09-07 20:02:01 +0530

    Add files via upload

---
+++ Bar Chart/index.js
@@ -0,0 +1,69 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { csv, arc, pie, scaleBand, scaleLinear, max, format } from 'd3';
+import { useData } from './useData';
+import { AxisBottom } from './AxisBottom';
+import { AxisLeft } from './AxisLeft';
+import { Marks } from './Marks';
+
+const width = 960;
+const height = 500;
+const margin = { top: 20, right: 30, bottom: 65, left: 220 };
+const xAxisLabelOffset = 50;
+
+const App = () => {
+  const data = useData();
+
+  if (!data) {
+    return <pre>Loading...</pre>;
+  }
+
+  const innerHeight = height - margin.top - margin.bottom;
+  const innerWidth = width - margin.left - margin.right;
+
+  const yValue = d => d.Country;
+  const xValue = d => d.Population;
+
+  const siFormat = format('.2s');
+  const xAxisTickFormat = tickValue => siFormat(tickValue).replace('G', 'B');
+
+  const yScale = scaleBand()
+    .domain(data.map(yValue))
+    .range([0, innerHeight])
+    .paddingInner(0.15);
+
+  const xScale = scaleLinear()
+    .domain([0, max(data, xValue)])
+    .range([0, innerWidth]);
+
+  return (
+    <svg width={width} height={height}>
+      <g transform={`translate(${margin.left},${margin.top})`}>
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickFormat={xAxisTickFormat}
+        />
+        <AxisLeft yScale={yScale} />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + xAxisLabelOffset}
+          textAnchor="middle"
+        >
+          Population
+        </text>
+        <Marks
+          data={data}
+          xScale={xScale}
+          yScale={yScale}
+          xValue={xValue}
+          yValue={yValue}
+          tooltipFormat={xAxisTickFormat}
+        />
+      </g>
+    </svg>
+  );
+};
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
